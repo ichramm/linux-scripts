8642efb780c0d8674febac88d39b7feff15c72b2
ichramm <jramirez.uy@gmail.com>
2019-09-14 19:38:14 -0300

    Bin files

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+FORCE=
+if [ "$1" = "-f" ]; then
+  FORCE=f
+fi
+
+for dotfile in $DIR/dotfiles/*; do
+  test -f $dotfile && ln -vs$FORCE $dotfile $HOME/.$(basename $dotfile)
+done
+
+for file in $DIR/bin/*; do
+  ln -vs$FORCE $file $HOME/bin/$(basename $file)
+done
+
